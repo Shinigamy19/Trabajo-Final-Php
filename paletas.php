b91fc6aaa54dbfb14426ce90c9124584c6343744
Eros Benitez <67855699+Shinigamy19@users.noreply.github.com>
2025-06-30 22:04:55 -0300

    Maquetas.
    Example de como quedaria.

--- paletas.php
+++ paletas.php
@@ -1,3 +1,59 @@
 <?php include __DIR__ . '/layout/header.php'; ?>
 
+
+  <!-- CONTENIDO -->
+  <div class="container my-5">
+    <h1>Pixel Art</h1>
+    <div class="row">
+      <!-- Card 1 -->
+      <div class="col-md-4 mb-4">
+        <div class="card border-info h-100">
+          <div class="card-header"><h4>Pj</h4></div>
+          <div class="card-body text-center">
+            <p>Usuario: Shinigamy19</p>
+            <img src="img\paleta_pj.png" alt="Pj">
+            <p class="mt-2">Sprite de personaje principal.</p>
+            <a href="#" class="card-link">Ver Perfil del Artista</a>
+          </div>
+          <div class="card-footer text-muted">
+            <h6>Hace 2 días</h6>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="col-md-4 mb-4">
+        <div class="card border-info h-100">
+          <div class="card-header"><h4>Paleta Copito</h4></div>
+          <div class="card-body text-center">
+            <p>Usuario: Shinigamy19</p>
+            <img src="img\paleta_copito.png" alt="Copito">
+            <p class="mt-2">Copito personaje secundario que ayuda a Pj.</p>
+            <a href="#" class="card-link">Ver Perfil del Artista</a>
+          </div>
+          <div class="card-footer text-muted">
+            <h6>Hace 2 días</h6>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="col-md-4 mb-4">
+        <div class="card border-info h-100">
+          <div class="card-header"><h4>Paleta Ojo Volador</h4></div>
+          <div class="card-body text-center">
+            <p>Usuario: Shinigamy19</p>
+            <img src="img\paleta_ojo.png" alt="Ojo Volador">
+            <p class="mt-2">Ojo volador enemigo de Pj.</p>
+            <a href="#" class="card-link">Ver Perfil del Artista</a>
+          </div>
+          <div class="card-footer text-muted">
+            <h6>Hace 2 días</h6> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 <?php include __DIR__ . '/layout/footer.php'; ?>
--- sonidos.php
+++ sonidos.php
@@ -1,3 +1,58 @@
 <?php include __DIR__ . '/layout/header.php'; ?>
 
+  <!-- CONTENIDO -->
+  <div class="container my-5">
+    <h1>Pixel Art</h1>
+    <div class="row">
+      <!-- Card 1 -->
+      <div class="col-md-4 mb-4">
+        <div class="card border-info h-100">
+          <div class="card-header"><h4>Efecto de sonido en 8 bits</h4></div>
+          <div class="card-body text-center">
+            <p>Usuario: Shinigamy19</p>
+            <audio src="audio/efecto.mp3" controls></audio>
+            <p class="card-text">Efecto de disparo de "Una conversacion con el roles".</p>
+            <a href="#" class="card-link">Ver Perfil del Artista</a>
+          </div>
+          <div class="card-footer text-muted">
+            <h6>Hace 2 días</h6>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 2 -->
+      <div class="col-md-4 mb-4">
+        <div class="card border-info h-100">
+          <div class="card-header"><h4>Cancion 3 en 8 bits</h4></div>
+          <div class="card-body text-center">
+            <p>Usuario: Shinigamy19</p>
+            <audio src="audio/cancion3.mp3" controls></audio>
+            <p class="card-text">Cancion 3 de "una conversacion con el roles".</p>
+            <a href="#" class="card-link">Ver Perfil del Artista</a>
+          </div>
+          <div class="card-footer text-muted">
+            <h6>Hace 2 días</h6>
+          </div>
+        </div>
+      </div>
+
+      <!-- Card 3 -->
+      <div class="col-md-4 mb-4">
+        <div class="card border-info h-100">
+          <div class="card-header"><h4>Cancion 23 en 8 bits</h4></div>
+          <div class="card-body text-center">
+            <p>Usuario: Shinigamy19</p>
+            <audio src="audio/cancion23.mp3" controls></audio>
+            <p class="card-text">Cancion 23 de "una conversacion con el roles".</p>
+            <a href="#" class="card-link">Ver Perfil del Artista</a>
+          </div>
+          <div class="card-footer text-muted">
+            <h6>Hace 2 días</h6> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 <?php include __DIR__ . '/layout/footer.php'; ?>
